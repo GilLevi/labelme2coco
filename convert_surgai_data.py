5541ef1083308bd965deca6c0f73a21ceeedc07e
Gil Levi <gil.levi100@gmail.com>
2022-07-13 18:15:57 +0100

    changed surgai coco annotations path

--- convert_surgai_data.py
+++ convert_surgai_data.py
@@ -1,9 +1,9 @@
 import labelme2coco
 # set directory that contains labelme annotations and image files
-labelme_folder = "../data/Hadassah_2021-07-05/raw_images/1/Frames"
+labelme_folder = "../data/Hadassah_2021-07-05/raw_images/4/Frames"
 
 # set export dir
-export_dir = "../data/Hadassah_2021-07-05/labelme2coco_data/coco_format_tests"
+export_dir = "../data/Hadassah_2021-07-05/labelme2coco_data/surgai_annotations_coco_format"
 
 # set train split rate
 train_split_rate = 1.0
