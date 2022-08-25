a9e13ef1f03d7f78e709449b7f2ff691d8505f09
Gil Levi <gil.levi100@gmail.com>
2022-08-25 18:36:27 +0100

    changed image conversion size to be general and not only 512

--- resize_annotations.py
+++ resize_annotations.py
@@ -4,8 +4,10 @@ from PIL import Image
 annotation_file = "../data/Hadassah_2021-07-05/raw_images_debug/3/Frames/frame0067.json"
 image_path = "../data/Hadassah_2021-07-05/raw_images_debug/3/Frames/frame0067.png"
 
-resize_annotation_file = "../data/Hadassah_2021-07-05/raw_images_debug_resized/3/Frames/frame0067.json"
-resze_image_path = "../data/Hadassah_2021-07-05/raw_images_debug_resized/3/Frames/frame0067.png"
+resize_annotation_file = "../data/Hadassah_2021-07-05/raw_images_debug_1500/3/Frames/frame0067.json"
+resize_image_path = "../data/Hadassah_2021-07-05/raw_images_debug_1500/3/Frames/frame0067.png"
+
+OUT_SIZE = 1500
 
 with open(annotation_file) as f:
     ann = json.load(f)
@@ -16,12 +18,12 @@ width, height = im.size
 
 for ann_shape in ann['shapes']:
     for i in range(len(ann_shape['points'])):
-        ann_shape['points'][i][0] *= (512 / width)
-        ann_shape['points'][i][1] *= (512 / height)
+        ann_shape['points'][i][0] *= (OUT_SIZE / width)
+        ann_shape['points'][i][1] *= (OUT_SIZE / height)
 
 
 with open(resize_annotation_file, 'w+') as f:
     json.dump(ann, f)
-im = im.resize((512, 512))
-im.save(resze_image_path)
+im = im.resize((OUT_SIZE, OUT_SIZE))
+im.save(resize_image_path)
 gil = 1
